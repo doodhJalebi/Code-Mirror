81c254e86f2e917e291f3d8f70720c2e891e17fa
Bahzad-Ahmed <54814375+Bahzad-Ahmed@users.noreply.github.com>
2020-06-20 22:38:16 +0500

    Update mirror.py; Runtime saves now reflected

--- mirror.py
+++ mirror.py
@@ -10,15 +10,17 @@ def mirror(src_dir,dst_dir):
     dst=merkletree(dst_dir)
     changes=Difference(src,dst,src.root_hash,dst.root_hash)
     for filename in changes:
-        if os.path.exists(os.path.join(dst_dir,filename))==True:
+        if os.path.isdir(os.path.join(dst_dir,filename)):
             shutil.rmtree(os.path.join(dst_dir, filename))
+        if os.path.isfile(os.path.join(dst_dir,filename)):
+            os.unlink(os.path.join(dst_dir, filename))
         if os.path.isdir(os.path.join(src_dir,filename))==True:
             shutil.copytree(os.path.join(src_dir,filename), os.path.join(dst_dir, filename))
         if os.path.isfile(os.path.join(src_dir,filename))==True:
             shutil.copy(os.path.join(src_dir,filename), os.path.join(dst_dir, filename))
         
         #os.popen('copy '+ os.path.join(src_dir,filename)+ os.path.join(dst_dir, filename))
-    time.sleep(5)
+    time.sleep(1)
     
 
 a_dir=r"C:\Users\DELL 5559\Desktop\a"
