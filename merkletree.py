d222aa906c472d3485eeb59f3f790f1bb0ff6e03
NihaMomin <46839375+NihaMomin@users.noreply.github.com>
2020-06-29 16:46:43 +0500

    Update merkletree.py

--- merkletree.py
+++ merkletree.py
@@ -1,38 +1,60 @@
 import os
+# Operating system library
+# To interface with the underlying operating system that Python is running on
+
 import hashlib
+# Python Library for generating secure hash
+
 import time
 
 
 class merkletree:
     def __init__(self, root_directory):
-        self.root = root_directory  # root directory of the mt folder
+        
+        self.root = root_directory 
+        # root directory of the merkel tree folder
+        
         self.merkletree = {}
-        self.hashes = dict()  # key: filename \\ value: hash
-        self.root_hash = ''  # merkle root hash
+        # MT is a dict containing hash of the respective files (as key) and the list conatining file name and its childrens dict (as value)
+        # Starting with files (childrens) and their respective hash, it later compiles the hash for the folder (parent) using children hashes (previous keys)
+
+        self.hashes = dict()  
+        # { filename -> key : hash -> value}
+
+        self.root_hash = ''  
+
+        # merkle root hash /  hash of the folder in the given root directory
         self.MerkleTree()
 
-    def buildMT(self):
-        for filename, hash in self.hashes.items():
+    def buildMT(self): # For generating merkel-tree dict
+        
+        for filename, hash in self.hashes.items():           
             items = self.getitems(filename)
+            # list of files that exists in the filename folder
             list = {}
+            # {hash : filename}
             temp = []
             temp.append(filename)
             for x in items:
+                # x -> name of file within the given folder
                 if filename == self.root:
                     list[self.hashes[x]] = x
                 else:
                     list[self.hashes[os.path.join(filename, x)]] = os.path.join(
                         filename, x)
             temp.append(list)
+            # temp is a list of  -> [filename , its dict of its child (file/es as values) and their hashes as keys if any exists]
             self.merkletree[hash] = temp
+
         self.root_hash = self.hashes[self.root]
 
     def MerkleTree(self):
         self.HashList(self.root)
         self.buildMT()
 
-    def hasher(self, data):
-        hash = hashlib.md5()
+    def hasher(self, data): # for generating hashes and encoding the data
+        
+        hash = hashlib.md5() # Hash Function
         path = os.path.join(self.root, data)
         hash_object = hashlib.md5(data.encode("utf-8"))
         if os.path.isfile(path):
@@ -42,6 +64,8 @@ class merkletree:
                 return "Error! Unable to open", path
             while True:
                 datachunk = openfile.read(8096)
+                # It reads chunks of the file into memory
+                # Anything above 1: means file is line buffered (say 8k.. ie: 8096, or higher) it does ^^
                 if not datachunk:
                     break
                 hash.update(datachunk.encode())
@@ -49,6 +73,8 @@ class merkletree:
         else:
             hash.update(data.encode())
         return hash.hexdigest()+hash_object.hexdigest()
+        # Returns the encoded data in hexadecimal format
+
 
     def getitems(self, dir):
         temp = []
@@ -56,25 +82,33 @@ class merkletree:
             dir = os.path.join(self.root, dir)
             # check whether the specified path is an existing directory
         if os.path.isdir(dir):
-            # a list containing the names of the entries in the directory
+            # if specified directory/path is an existing directory...
             lst = os.listdir(dir)
+            # ... it'll make a list containing the names of the entries (file/es) in the directory given by path
             for i in lst:
                 if i != "__pycache__":
                     temp.append(i)
             temp.sort()
         return temp
+        #  It returns sorted list of file/s that exists in the folder whose directory is given
+
 
     def childHL(self, root_directory):
         files = self.getitems(root_directory)
+        # Lists of files that exists in the given root directory
         if not files:
             self.hashes[root_directory] = ''
             return
         for file in files:
             filepath = os.path.join(root_directory, file)
             if os.path.isdir(filepath):
+                # if path is valid and the file exists in the given dir..
                 self.childHL(file)
+                # checking for further children of file (if any)
                 subfiles = self.getitems(file)
+                # subfile -> list of subfiles that exists in the given file
                 temp = ''
+            # making the dictionary of filename and its respective hash
                 for subfile in subfiles:
                     temp += self.hashes[os.path.join(file, subfile)]
                 if not subfiles:
@@ -92,6 +126,7 @@ class merkletree:
     def HashList(self, root_directory):
         self.childHL(root_directory)
         files = self.getitems(root_directory)
+        # Lists of files that exists in the given root directory
         if not files:
             self.hashes[root_directory] = ''
             return
@@ -99,6 +134,9 @@ class merkletree:
         for subfiles in files:
             temp += self.hashes[subfiles]
         self.hashes[root_directory] = self.hasher(temp)
+        # self.hashes -> dict { hash as key : filename as values }
+        # hashes (keys) of children files are alloted first then using those hashes
+        # folder name (parent) is hashed
 
 # change_list = []
 
