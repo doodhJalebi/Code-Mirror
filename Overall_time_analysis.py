421215bdb4c000229feb8f36f5874bcc11e6c97f
NihaMomin <46839375+NihaMomin@users.noreply.github.com>
2020-07-01 23:01:29 +0500

    Add files via upload

---
+++ Overall_time_analysis.py
@@ -0,0 +1,42 @@
+from mirror import *
+import matplotlib.pyplot as plt
+
+
+# a_dir = r"C:\Users\User\Desktop\Niha\DS2-project\folder\a"
+# b_dir = r"C:\Users\User\Desktop\Niha\DS2-project\folder\b"
+
+# while True:
+#     try:
+#         start_time = time.time()
+#         mirror(a_dir, b_dir)
+#         end_time = time.time()
+#         print("Total time = ", end_time - start_time)
+#         break
+#     except:
+#         pass
+
+
+# Values of x and y are plotted after testing each case
+# y-axis values 
+y =  [0,100,200,300,400,500, 600, 700, 800, 900, 1000]
+# x-axis values 
+x =  [1.008073091506958,1.0302975177764893,3.9004528522491455,6.805618524551392,6.246065139770508,
+ 7.656027317047119,7.5103678703308105,11.54430866241455,13.878087520599365,16.903316020965576,
+ 18.9206485748291]
+  
+# plotting points as a scatter plot 
+plt.scatter(x, y, label= "stars", color= "red",  
+            marker= "*", s=30) 
+  
+# x-axis label 
+plt.xlabel('Time Taken In Mirroring') 
+# y-axis label 
+plt.ylabel('Number of Files in Source Folder') 
+# plot title 
+plt.title('Code Mirror - Time Graph') 
+# showing legend 
+plt.legend() 
+  
+# function to show the plot 
+plt.show() 
+
